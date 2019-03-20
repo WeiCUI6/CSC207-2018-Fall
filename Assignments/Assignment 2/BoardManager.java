91c87e76e106e273508b9ed9266d621fb4d61335
Wei CUI <46061514+WeiCUI6@users.noreply.github.com>
2019-03-19 21:32:06 -0400

    Upload a2 java code

---
+++ Assignments/Assignment 2/BoardManager.java
@@ -0,0 +1,114 @@
+package fall2018.csc2017.slidingtiles;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Manage a board, including swapping tiles, checking for a win, and managing taps.
+ */
+class BoardManager implements Serializable {
+
+    /**
+     * The board being managed.
+     */
+    private Board board;
+
+    /**
+     * Manage a board that has been pre-populated.
+     * @param board the board
+     */
+    BoardManager(Board board) {
+        this.board = board;
+    }
+
+    /**
+     * Return the current board.
+     */
+    Board getBoard() {
+        return board;
+    }
+
+    /**
+     * Manage a new shuffled board.
+     */
+    BoardManager() {
+        List<Tile> tiles = new ArrayList<>();
+        final int numTiles = Board.NUM_ROWS * Board.NUM_COLS;
+        for (int tileNum = 0; tileNum != numTiles; tileNum++) {
+            tiles.add(new Tile(tileNum));
+        }
+
+        Collections.shuffle(tiles);
+        this.board = new Board(tiles);
+    }
+
+    /**
+     * Return whether the tiles are in row-major order.
+     *
+     * @return whether the tiles are in row-major order
+     */
+    boolean puzzleSolved() {
+        boolean solved = true;
+        Iterator<Tile> boardIterator = board.iterator();
+        for (int i = 1; i != board.numTiles() + 1; i++) {
+            Tile tempTile = boardIterator.next();
+            if (i != tempTile.getId()) {
+                solved = false;
+            }
+        }
+        return solved;
+    }
+
+    /**
+     * Return whether any of the four surrounding tiles is the blank tile.
+     *
+     * @param position the tile to check
+     * @return whether the tile at position is surrounded by a blank tile
+     */
+    boolean isValidTap(int position) {
+
+        int row = position / Board.NUM_COLS;
+        int col = position % Board.NUM_COLS;
+        int blankId = board.numTiles();
+        // Are any of the 4 the blank tile?
+        Tile above = row == 0 ? null : board.getTile(row - 1, col);
+        Tile below = row == Board.NUM_ROWS - 1 ? null : board.getTile(row + 1, col);
+        Tile left = col == 0 ? null : board.getTile(row, col - 1);
+        Tile right = col == Board.NUM_COLS - 1 ? null : board.getTile(row, col + 1);
+        return (below != null && below.getId() == blankId)
+                || (above != null && above.getId() == blankId)
+                || (left != null && left.getId() == blankId)
+                || (right != null && right.getId() == blankId);
+    }
+
+    /**
+     * Process a touch at position in the board, swapping tiles as appropriate.
+     *
+     * @param position the position
+     */
+    void touchMove(int position) {
+
+        int row = position / Board.NUM_ROWS;
+        int col = position % Board.NUM_COLS;
+        int blankId = board.numTiles();
+
+        // tiles is the blank tile, swap by calling Board's swap method.
+        Iterator<Tile> boardIterator = board.iterator();
+        Tile tempTile = boardIterator.next();
+        int i = 0;
+        if (isValidTap(position)) {
+            while (tempTile.getId() != blankId) {
+                i++;
+                tempTile = boardIterator.next();
+            }
+        }
+
+        int tempRow = i / Board.NUM_ROWS;
+        int tempCol = i % Board.NUM_COLS;
+        board.swapTiles(tempRow, tempCol, row, col);
+    }
+
+}
